7564f42f3b0493c40ce5a6d49d746898d39d887b
Mihai <serbanmihaigeorge@gmail.com>
2025-07-23 18:44:06 +0200

    normalized routing structure

--- server/Controllers/choreControllers.js
+++ server/Controllers/choreControllers.js
@@ -1,5 +1,20 @@
 const Chore = require('../Models/choreModels.js');
 
+function parseName(name) {
+  name = name.toLowerCase();
+  let result = '';
+  for (let i = 0; i < name.length; i++) {
+    if (name[i-1] === undefined || name[i-1] === '-') {
+      result += name[i].toUpperCase();
+    } else {
+      result += name[i];
+    }
+  }
+  result = result.replaceAll('-', ' ');
+
+  return result;
+}
+
 exports.getAllChores = async function (request, response) {
   try {
     const data = await Chore.find({});
@@ -37,7 +52,7 @@ exports.markChoreComplete = async function (request, response) {
     response.status(400).send('Bad Request, Name is required and must be a string');
   } else {
     try {
-      const data = await Chore.findOneAndUpdate({ name: name}).set('isDone', true);
+      const data = await Chore.findOneAndUpdate({ name: parseName(name) }).set('isDone', true);
       response.status(200).send(`${data.name} marked as complete`);
     } catch (error) {
       response.status(400).send('Something went wrong while marking chore as completed');
@@ -68,8 +83,22 @@ exports.assignChore = async function (request, response) {
     response.status(400).send('Bad Request, Name or User is required and must be a string');
   } else {
     try {
-      const data = await Chore.findOneAndUpdate({ name: name }).set('assignee', user);
+      const data = await Chore.findOneAndUpdate({ name: parseName(name) }).set('assignee', user);
       response.status(200).send(`${data.name} assigned to ${user}`);
+    } catch (error) {
+      response.status(400).send('Something went wrong while assigning chore');
+    }
+  }
+};
+
+exports.unassignChore = async function (request, response) {
+  const { name } = request.params;
+  if (!name || typeof name !== 'string') {
+    response.status(400).send('Bad Request, Name is required and must be a string');
+  } else {
+    try {
+      const data = await Chore.findOneAndUpdate({ name: name }).set('assignee', 'Unassigned');
+      response.status(200).send(`${data.name} Unassigned`);
     } catch (error) {
       response.status(400).send('Something went wrong while assigning chore');
     }
--- server/Router/choreRouter.js
+++ server/Router/choreRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getAllChores, generateChore, markChoreComplete, markChoreNotComplete, assignChore } = require('../Controllers/choreControllers.js')
+const { getAllChores, generateChore, markChoreComplete, markChoreNotComplete, assignChore, unassignChore } = require('../Controllers/choreControllers.js')
 
 router.get('/chores', getAllChores);
 router.post('/chores', generateChore);
-router.put('/chores/markComplete/:name', markChoreComplete);
-router.put('/chores/markNotComplete/:name', markChoreNotComplete);
+router.put('/chores/markcomplete/:name', markChoreComplete);
+router.put('/chores/marknotcomplete/:name', markChoreNotComplete);
 router.put('/chores/assign/:name', assignChore);
+router.put('/chores/unassign/:name', unassignChore);
 
 module.exports = router;
