5570f0426b602dccd13470ba097d6e452514c767
luanadefourny <luanadefourny@gmail.com>
2025-08-02 16:22:38 +0100

    refactored App.tsx to typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import LandingPage from './Components/LandingPage/LandingPage';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import ChoreList from './Components/ChoreList/ChoreList';
 import { useState } from 'react';
-import { getCurrentUser } from './Services/APIClient';
 
 export default function App() {
   const [user, setUser] = useState(''); //user.ts interface
@@ -12,7 +11,7 @@ export default function App() {
     <div className="body-container">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage user={user} setUser={setUser}/>} />
+          <Route path="/" element={<LandingPage setUser={setUser}/>} />
           <Route path=":user/chores" element={<ChoreList />} />
         </Routes>
       </BrowserRouter>
